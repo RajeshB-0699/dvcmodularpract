119b9cc6e07c50af56dd5771e28aba7842f74f12
RajeshB-0699 <rajeshbala802@gmail.com>
2025-05-06 10:18:25 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='dvc pipeline using modular code mlops water potability',
+    author='Rajesh',
+    license='',
+)
